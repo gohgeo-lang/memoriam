82bbc4267807de53e3588842c5b3fd155365b330
SIH <lordangel82@gmail.com>
2025-09-20 17:53:12 +0900

    guide 컴포넌트 추가, npm install react-icons 설치

--- src/pages/Guide/Guide.jsx
+++ src/pages/Guide/Guide.jsx
@@ -1,6 +1,40 @@
 import "./Guide.css";
+import MissionVision from "../../components/Guide/MissionVision";
+import ProcedureGuide from "../../components/Guide/ProcedureGuide";
+import ServiceGuide from "../../components/Guide/ServiceGuide";
+import CostGuide from "../../components/Guide/CostGuide";
+import SupportGuide from "../../components/Guide/SupportGuide";
+import FAQ from "../../components/Guide/FAQ";
+import CTASection from "../../components/Guide/CTASection";
+import HeroSection from "../../components/Guide/HeroSection";
+
 export default function Guide() {
-  return <div></div>;
+  return (
+    <>
+      <HeroSection />
+
+      {/* 절차 안내 */}
+      <ProcedureGuide />
+
+      {/* 서비스 안내 */}
+      <ServiceGuide />
+
+      {/* 비용 안내 */}
+      <CostGuide />
+
+      {/* 심리 상담 */}
+      <SupportGuide />
+
+      {/* FAQ */}
+      <FAQ />
+
+      {/* 미션/비전 지우기 또는 위치 변경 */}
+      <MissionVision />
+
+      {/* 상담 CTA */}
+      <CTASection />
+    </>
+  );
 }
 
 {
